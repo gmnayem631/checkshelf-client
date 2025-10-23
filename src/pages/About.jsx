46a49fbea60cd1a4389e573dd811ffec0c0174b9
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-10-23 22:09:34 +0600

    added the coaches data in the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import coaches from "../../public/coaches.json";
 
 const About = () => {
   return (
     <div className="max-w-7xl mx-auto">
       <h2 className="text-7xl text-center my-3">Meet Our Chess Masters</h2>
+      <div className="grid grid-cols-3 gap-5 mt-5">
+        {coaches.map((coach, idx) => {
+          return (
+            <div key={idx}>
+              <img src={coach.image} alt="" className="h-96 rounded-2xl" />
+              <p className="font-bold">{coach.name}</p>
+              <p>{coach.type}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
