9b72414839e324f5b52f4ceb27864cd80c212823
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-10-23 21:57:26 +0600

    started editing the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const About = () => {
-  return <div className="max-w-7xl mx-auto">About Page</div>;
+  return (
+    <div className="max-w-7xl mx-auto">
+      <h2 className="text-7xl text-center my-3">Meet Our Chess Masters</h2>
+    </div>
+  );
 };
 
 export default About;
