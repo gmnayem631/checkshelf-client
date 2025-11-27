d42e8b8661546096e9023bdce14ae277e6c3cfe6
gmnayem631 <gmnayem631@gmail.com>
2025-11-27 20:19:47 +0600

    installed react router and set up the router

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <main>
+        <Outlet></Outlet>
+      </main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="max-w-7xl mx-auto">Home Page</div>;
+};
+
+export default Home;
