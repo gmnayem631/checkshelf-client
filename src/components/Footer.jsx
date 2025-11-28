688307d781742406e7ec706b6de16ba928507d4b
gmnayem631 <gmnayem631@gmail.com>
2025-11-28 18:20:24 +0600

    added the footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+      <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Courses</a>
+        <a className="link link-hover">Books</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+      </nav>
+      <form>
+        <h6 className="footer-title">Newsletter</h6>
+        <fieldset className="w-80">
+          <label>Enter your email address</label>
+          <div className="join my-4">
+            <input
+              type="text"
+              placeholder="username@site.com"
+              className="input input-bordered join-item"
+            />
+            <button className="btn btn-primary join-item">Subscribe</button>
+          </div>
+        </fieldset>
+      </form>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
@@ -11,7 +12,9 @@ const MainLayout = () => {
       <main>
         <Outlet></Outlet>
       </main>
-      <footer></footer>
+      <footer>
+        <Footer></Footer>
+      </footer>
     </div>
   );
 };
