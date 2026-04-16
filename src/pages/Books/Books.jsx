e52d291e553285cc0b477ca8181e694495f8e3dc
gmnayem631 <gmnayem631@gmail.com>
2026-04-16 14:10:03 +0600

    added sort functionality in the Books page

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -7,6 +7,7 @@ const Book = () => {
 
   const [books, setBooks] = useState(initialBooks);
   const [search, setSearch] = useState("");
+  const [sortOption, setSortOption] = useState("");
 
   const filteredBooks = books.filter(
     (book) =>
@@ -14,9 +15,16 @@ const Book = () => {
       book.author.toLowerCase().includes(search.toLowerCase()),
   );
 
+  const sortedBooks = [...filteredBooks];
+  if (sortOption === "price") {
+    sortedBooks.sort((a, b) => a.price - b.price);
+  } else if (sortOption === "pages") {
+    sortedBooks.sort((a, b) => a.pages - b.pages);
+  }
+
   return (
     <div className="max-w-11/12 mx-auto my-20">
-      <h2 className="text-center text-2xl md:text-4xl my-5">
+      <h2 className="text-center text-3xl md:text-4xl my-5 md:font-semibold">
         Order and Buy Books
       </h2>
 
@@ -29,7 +37,7 @@ const Book = () => {
               onChange={(e) => setSearch(e.target.value)}
               type="text"
               value={search}
-              placeholder="Search by book name, author, or tag..."
+              placeholder="Search by book name, or author..."
               className="input input-bordered w-full focus:outline-none focus:ring-2 focus:ring-[#fefce8] rounded-lg"
             />
           </div>
@@ -37,18 +45,19 @@ const Book = () => {
           {/* Filter Dropdown */}
           <div className="w-full md:w-52">
             <select
-              defaultValue="Sort by (Low to High)"
-              className="select select-bordered w-full focus:outline-none focus:ring-2 focus:ring-[#fefce8] rounded-lg"
+              value={sortOption}
+              onChange={(e) => setSortOption(e.target.value)}
+              className="select select-bordered w-full"
             >
-              <option disabled={true}>Sort by (Low to High)</option>
-              <option>Price</option>
-              <option>Number of Pages</option>
+              <option value="">Sort by</option>
+              <option value="price">Price</option>
+              <option value="pages">Number of Pages</option>
             </select>
           </div>
         </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {filteredBooks.map((book) => {
+        {sortedBooks.map((book) => {
           return <BookCard key={book._id} book={book}></BookCard>;
         })}
       </div>
