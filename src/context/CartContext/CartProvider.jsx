5722fc172948be5629ad1a3da636de5b30423d5a
gmnayem631 <gmnayem631@gmail.com>
2026-05-09 14:48:42 +0600

    added context API for the cart system

---
+++ src/context/CartContext/CartProvider.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { CartContext } from "./CartContext";
+import {
+  getStoredCart,
+  addToStoredCart,
+  removeFromStoredCart,
+  deleteStoredCart,
+} from "../utils/cartStorage";
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState(getStoredCart());
+
+  // Add to Cart
+  const addToCart = (book) => {
+    const updatedCart = addToStoredCart(book);
+    setCart([...updatedCart]);
+  };
+
+  // Remove item
+  const removeFromCart = (id) => {
+    const updatedCart = removeFromStoredCart(id);
+    setCart([...updatedCart]);
+  };
+
+  // Clear the whole cart
+  const clearCart = () => {
+    deleteStoredCart();
+    setCart([]);
+  };
+
+  const cartCount = cart.reduce((total, item) => total + item.quantity, 0);
+  const totalPrice = cart.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0,
+  );
+
+  const cartInfo = {
+    cart,
+    addToCart,
+    removeFromCart,
+    clearCart,
+    cartCount,
+    totalPrice,
+  };
+
+  return (
+    <CartContext.Provider value={cartInfo}>{children}</CartContext.Provider>
+  );
+};
+
+export default CartProvider;
